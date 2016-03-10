3d2a014c9f27c5808951834d810e1612163f5826
Trine Schmidt <trineschmidt@Trines-MacBook-Pro.local>
2016-03-10 09:36:22 +0100

    adding files

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@
 // This is changing the color of the selected navigation item - adding and removing class "selected" with the onclick event
 // Using css and calling fx "li.current a" we can change the original color, size, etc so something new with the click - Test it
 $(function(){
-    $('ul li a').on('click', function(){
+    $('ul li a').on('click', function()
+    {
         $(this).parent().addClass('current').siblings().removeClass('current');
-        });
+    }
+    );
     }); 
 
 // Toggling/switching/pushing the contentarea and mobile navigation into sight - and out again : Using toggleClass to add and remove the Class "active"
